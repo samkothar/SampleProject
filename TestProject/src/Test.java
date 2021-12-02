38bb3408857f23c68ee46d17d6d06dfc19557822
sakothar <sakothar@LIN51018387.corp.capgemini.com>
2021-12-02 10:16:18 +0530

    Added welcome message

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello!! Welcome to Capgemini");
+		System.out.println("Hello!! Welcome to Capgemini World");
 	}
 
 }
